a6c2992507e5c46e0937e8f4fdb71b2707d6971e
SEOWOO <subi9807@gmail.com>
2026-05-14 13:03:54 +0900

    chore(ios): change bundle id to com.divergram.app.ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.divergram.app',
+  appId: 'com.divergram.app.ios',
   appName: 'Divergram',
   webDir: 'dist',
   server: {
