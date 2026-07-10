944c131532196684167e17e34a1e043464abc4d4
SEOWOO <subi9807@gmail.com>
2026-07-10 12:36:23 +0900

    bump release build numbers

--- app/app.config.ts
+++ app/app.config.ts
@@ -71,7 +71,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.divergram.app.ios',
-      buildNumber: '31',
+      buildNumber: '32',
       googleServicesFile: './GoogleService-Info.plist',
       usesAppleSignIn: true,
       infoPlist: {
@@ -108,7 +108,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       },
       package: 'com.divergram.app',
       googleServicesFile: './google-services.json',
-      versionCode: 28,
+      versionCode: 29,
       permissions: [
         'ACCESS_FINE_LOCATION',
         'ACCESS_COARSE_LOCATION',
